b243da1441ca6116a091f9790c3e2ef971157472
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-04-28 17:01:45 +1000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.jsx',
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer : {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3000,
+        watchOptions: { poll: 2000 }
+    }
+}
