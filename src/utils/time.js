65717847f592faf87fc701f886f7a2c6a5f86f03
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-01 15:16:04 +1000

    time utils to convert seconds to 00:00:00 format

---
+++ src/utils/time.js
@@ -0,0 +1,28 @@
+/**
+ * Returns time in format "h:mm:ss" 
+ * based on given `time` argument.
+ * If there is no hours data, they will be omit.
+ * @param {number} time Time in seconds
+ */
+export function formatTime(time) {
+    // time is NaN
+    if (isNaN(time)) {
+        return '00:00'
+    // or time is not finite
+    } else if (!isFinite(time)) {
+        return '00:00'
+    } else {
+        // NOTE: part "|0" truncates float to integer
+        const tt = Math.round(time);
+        const hours = (tt / 3600) | 0;
+        const minutes = ((tt % 3600) / 60) | 0; // 3721
+        const seconds = tt % 60;
+
+        // dont write hourse if there are no them
+        const hh = hours > 0 ? `${hours}:` : '';
+        const mm = minutes > 9 ? `${minutes}` : `0${minutes}`;
+        const ss = seconds > 9 ? `${seconds}` : `0${seconds}`;
+
+        return `${hh}${mm}:${ss}`
+    }
+}
