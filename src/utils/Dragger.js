8c0432ac8194cd181171c47bdc6ab942b63139c0
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-04 00:02:31 +1000

    finish Dragger

--- src/utils/Dragger.js
+++ src/utils/Dragger.js
@@ -10,12 +10,16 @@ export default class Dragger {
         this.initiators = initiators
         this.enabled = false;
         this.inDrag = false;
+
+        this.onDocumentMouseDown = this.onDocumentMouseDown.bind(this)
+        this.onDrag = this.onDrag.bind(this)
+        this.stopDrag = this.stopDrag.bind(this)
     }
 
     enable() {
         if (this.enabled) return;
 
-        document.addEventListener('mousedown', this.onDocumentMouseDown.bind(this));
+        document.addEventListener('mousedown', this.onDocumentMouseDown);
         
         this.enabled = true;
     }
@@ -32,7 +36,7 @@ export default class Dragger {
                 // this should disable text selection
                 e.preventDefault()
                 // no one will recieve this event
-                e.stopPropagation()
+                e.stopImmediatePropagationn()
                 this.startDrag(e)
             }
         }
@@ -43,10 +47,43 @@ export default class Dragger {
      * @param {MouseEvent} e 
      */
     startDrag(e) {
-        console.log('drag started')
         this.inDrag = true;
+
         // calc offset
+        const bounds = this.target.getBoundingClientRect()
+        this.offsetX = e.pageX - bounds.left;
+        this.offsetY = e.pageY - bounds.top;
+        
         // set styles
-        // add listeners to move and up events
+        const compStyle = window.getComputedStyle(this.target)
+        const left = compStyle.left;
+        const top = compStyle.top;
+        const style = this.target.style;
+        // reset right and bottom rules
+        // so they dont affect element's position 
+        style.right = null;
+        style.bottom = null;
+        style.left = left;
+        style.top = top;
+
+        // set listeners 
+        // document.removeEventListener('mousedown', this.onDocumentMouseDown);
+        document.addEventListener('mousemove', this.onDrag);
+        document.addEventListener('mouseup', this.stopDrag);
+    }
+
+    onDrag(e) {
+        const x = e.pageX - this.offsetX;
+        const y = e.pageY - this.offsetY;
+        this.target.style.left = `${x}px`;
+        this.target.style.top = `${y}px`;
+    }
+
+    stopDrag(e) {
+        if (!this.inDrag || e.button != 0) return;
+
+        document.removeEventListener('mousemove', this.onDrag)
+        document.removeEventListener('mouseup', this.stopDrag)
+        this.inDrag = false
     }
 }
