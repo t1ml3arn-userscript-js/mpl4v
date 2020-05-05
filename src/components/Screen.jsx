a8c8d67c211c451b8c98514deb14863091a177c4
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-05 18:44:44 +1000

    better Screen classes

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -3,9 +3,12 @@ import PropType from 'prop-types'
 
 export default function Screen(props) {
     const {showScreen, requestFullscreen, fullscreen} = props
+    const dragIniter = fullscreen ? "" : "mpl4v-drag-initiator"
+    const hidden = showScreen ? '' : 'mpl4v--hidden'
+    
     return (
     <div 
-        className={`mpl4v-screen mpl4v-drag-initiator ${showScreen ? '' : 'mpl4v--hidden'}`}
+        className={`mpl4v-screen ${dragIniter} ${hidden}`}
         data-fullscreen={ fullscreen }
         onDoubleClick={ requestFullscreen }
     ></div>
