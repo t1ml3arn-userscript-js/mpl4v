00fe3e99063f9250269bec388d5472dfe10c32a1
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-06 00:32:48 +1000

    upd exit fullscreen icon

--- src/components/ScreenButton.jsx
+++ src/components/ScreenButton.jsx
@@ -3,14 +3,19 @@ import PropTypes from 'prop-types'
 
 export default function ScreenButton(props) {
     const {showScreen, toogleScreen, fullscreen} = props;
+    
     let title = ''
     if (fullscreen)         title = 'Exit Fullscreen Mode'
     else if (showScreen)    title = 'Fold Screen'
     else                    title = 'Show Screen'
 
+    const iconClass = fullscreen ? "zmdi-fullscreen-exit" 
+        : showScreen ? "zmdi-unfold-less" 
+        : "zmdi-unfold-more"
+
     return (
     <i 
-        className={`zmdi mpl4v-fullscreen ${showScreen ? "zmdi-unfold-less" : "zmdi-unfold-more"}`} 
+        className={`zmdi mpl4v-fullscreen ${iconClass}`} 
         onClick={toogleScreen}
         title={ title }
     ></i>
