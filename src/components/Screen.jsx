ba950474730202089ce616606fdd50515920bf42
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-05 14:49:16 +1000

    make Screen stateless

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,48 +1,14 @@
 import React from 'react'
-import fscreen from 'fscreen'
 import PropType from 'prop-types'
 
-export default class Screen extends React.Component {
-    constructor(props) {
-        super(props)
-        
-        // this.screenRef = React.createRef();
-    }
-
-    requestFullscreen = e => {
-        const result = fscreen.requestFullscreen(e.target)
-        // if there is a result, it is Promise
-        if (result)
-            result.then(this.fullscreenSucceeded, this.fullscreenFailed)
-        else {
-            // TODO lift this fullscreen dancing up
-            // and control it with event handler, not a Promise
-            // opera and safari does not return promise
-            document.fullscreenEnabled 
-                ? this.fullscreenSucceeded() 
-                : this.fullscreenFailed()
-        }
-    }
-
-    fullscreenSucceeded = () => {
-        console.log(this, 'succeeded')
-        this.props.enterFullscreen()
-    }
-
-    fullscreenFailed = e => {
-        console.log('fullscreen failed with error:', e)
-    }
-
-    render() {
-        const {showScreen} = this.props
-        return (
-        <div 
-            className={`mpl4v-screen mpl4v-drag-initiator ${showScreen ? '' : 'mpl4v--hidden'}`}
-            onDoubleClick={ this.requestFullscreen }
-        >
-        </div>
-        )
-    }
+export default function Screen(props) {
+    const {showScreen, requestFullscreen} = props
+    return (
+    <div 
+        className={`mpl4v-screen mpl4v-drag-initiator ${showScreen ? '' : 'mpl4v--hidden'}`}
+        onDoubleClick={ requestFullscreen }
+    ></div>
+    )
 }
 
 Screen.propTypes = {
