ff54d2d823614d7407531740189fed493c112a13
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-05 15:57:07 +1000

    fix indentation

--- src/index.jsx
+++ src/index.jsx
@@ -80,24 +80,26 @@ class App extends React.Component {
             />
             <div class="mpl4v-controls" data-fullscreen={ fullscreen }>
                 <Progress progress={ this.state.progress } onProgressChange={ this.updateProgress }/>
-                    <div class="mpl4v-control-btns mpl4v-drag-initiator">
-                        <div class="mpl4v-fl-row mpl4-controls--left ">
-                            <i class="zmdi zmdi-repeat"></i>
-                            <i class="zmdi zmdi-skip-previous"></i>
-                            <i class="zmdi zmdi-play"></i>
-                            <i class="zmdi zmdi-skip-next"></i>
-                            <i class="zmdi zmdi-shuffle"></i>
-                        </div>
-                        <TimeLabel time={12345} duration={44444}/>
-                        <div class="mpl4v-fl-row mpl4-controls--right">
-                            <i class="zmdi zmdi-volume-up mpl4v-vol-ctrl"></i>
-                            <ScreenButton 
-                                showScreen={this.state.showScreen}
-                                toogleScreen={this.toogleScreen}
-                            />
-                            <i class="zmdi zmdi-download"></i>
-                        </div>
+                <div class="mpl4v-control-btns mpl4v-drag-initiator">
+                    <div class="mpl4v-fl-row mpl4-controls--left ">
+                        <i class="zmdi zmdi-repeat"></i>
+                        <i class="zmdi zmdi-skip-previous"></i>
+                        <i class="zmdi zmdi-play"></i>
+                        <i class="zmdi zmdi-skip-next"></i>
+                        <i class="zmdi zmdi-shuffle"></i>
                     </div>
+                    <TimeLabel time={12345} duration={44444}/>
+                    <div class="mpl4v-fl-row mpl4-controls--right">
+                        <i class="zmdi zmdi-volume-up mpl4v-vol-ctrl"></i>
+                        <ScreenButton 
+                            <ScreenButton 
+                        <ScreenButton 
+                            showScreen={this.state.showScreen}
+                            toogleScreen={this.toogleScreen}
+                        />
+                        <i class="zmdi zmdi-download"></i>
+                    </div>
+                </div>
             </div>
         </div>
         )
