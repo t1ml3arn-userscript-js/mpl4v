de55a00561891ad92ae03ce78d184801b859d0da
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-06 17:38:11 +1000

    move Bar to its own file

---
+++ src/components/Bar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types'
+
+export function Bar(props) {
+    const { progress, classes } = props;
+    const styles = {
+        width: `${progress}%`,
+    };
+    // what is common for hor and vert bars ?
+    /*
+    .bar--hor           { width: value; height: value }
+    .indicator--hor     { width: calc(); height: value }
+
+    .bar--vert          { width: value; height: value }
+    .indicator--vert    { width: value; height: calc() }
+    */
+    return <div className={`mpl4v-playback-progressbar__bar mpl4v-bar--hor ${classes}`} style={styles}></div>;
+}
+
+Bar.defaultProps = {
+    classes: ""
+}
+
+Bar.propTypes = {
+    progress: PropTypes.number.isRequired,
+    classes: PropTypes.string
+}
--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types'
+import { Bar } from "./Bar";
 
 export default class PlaybackProgress extends Component {
     constructor(props) {
@@ -74,34 +75,6 @@ PlaybackProgress.propTypes = {
     progress: PropTypes.number.isRequired,
     onProgressChange: PropTypes.func.isRequired,
     isHorizontal: PropTypes.bool.isRequired
-}
-
-function Bar(props) {
-    const { progress, classes } = props;
-
-    const styles = {
-        width: `${progress}%`,
-    };
-
-    // what is common for hor and vert bars ?
-    /* 
-    .bar--hor           { width: value; height: value }
-    .indicator--hor     { width: calc(); height: value }
-
-    .bar--vert          { width: value; height: value }
-    .indicator--vert    { width: value; height: calc() }
-    */
-
-    return <div className={ `mpl4v-playback-progressbar__bar mpl4v-bar--hor ${classes}` } style={ styles }></div>
-}
-
-Bar.defaultProps = {
-    classes: ""
-}
-
-Bar.propTypes = {
-    progress: PropTypes.number.isRequired,
-    classes: PropTypes.string
 }
 
 class ProgressCalculator {
