c5b2e1eefd53c9a114c2cdf41440208b0b508dfc
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-06 17:22:40 +1000

    improve PlaybackProgress appearance

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -47,6 +47,12 @@ export default class PlaybackProgress extends Component {
 
     render() {
         const { progress } = this.props;
+        const headStyle = {
+            left: `${progress}%`,
+            // Below is a way to contain the head only
+            // inside the bar, but I must know head's size.
+            // marginLeft: `-${Math.round(progress*0.1)}px`
+        }
         
         return (
         <div 
@@ -54,9 +60,11 @@ export default class PlaybackProgress extends Component {
             ref={ this.barEltRef }
             onMouseDown={ this.startSeek }
         >
+            <div className={ "mpl4v-playback-progressbar__underlay" }/>
             <Bar classes={ "mpl4v-bar-buff-color"} progress={ 90 }/>
             <Bar classes={ "mpl4v-bar-seek-color"} progress={ 60 }/>
             <Bar classes={ "mpl4v-bar-progress-color"} progress={ progress }/>
+            <div style={ headStyle } className={ "mpl4v-playback-progressbar__head" }></div>
         </div>
         )
     }
