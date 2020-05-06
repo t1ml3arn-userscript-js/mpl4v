5ca97d3245dfed76d94ecf023f6598c71d7eef94
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-07 00:35:54 +1000

    PlaybackProgress extends ProgressBar basic component

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,63 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types'
 import Bar from "./Bar";
-import ProgressCalculator from "../utils/ProgressCalculator";
+import ProgressBar from './ProgressBar'
 
-export default class PlaybackProgress extends Component {
+export default class PlaybackProgress extends ProgressBar {
     static propTypes = {
         progress: PropTypes.number.isRequired,
         onProgressChange: PropTypes.func.isRequired,
-        isHorizontal: PropTypes.bool.isRequired
     }
-    
+
     constructor(props) {
         super(props)
-
-        this.state = {
-            seek: false,
-        }
-        this.calculator = new ProgressCalculator()
-        this.barEltRef = React.createRef()
-    }
-
-    startSeek = event => {
-        // this should deisable text selection
-        event.preventDefault()
-        // I dont want this event to be heared
-        // outside this component
-        event.stopPropagation(); 
-
-        this.calculator.init(event.nativeEvent, this.barEltRef.current, this.props.isHorizontal);
-        // calc current progress in percents
-        const progress = this.calculator.getProgress();
-
-        // lift progress value up
-        this.props.onProgressChange(Math.round(progress));
-
-        // enable control
-        document.addEventListener('mouseup', this.stopSeek);
-        document.addEventListener('mousemove', this.seek);
-
-        this.setState({ seek: true })
-    }
-    
-    stopSeek = event => {
-        document.removeEventListener('mouseup', this.stopSeek);
-        document.removeEventListener('mousemove', this.seek);
-
-        this.setState({ seek: false })
-    }
-
-    seek = event => {
-        this.calculator.update(event);
-        // calc current progress in percents
-        let progress = this.calculator.getProgress()
-        
-        // bound it in range [0, 100]
-        progress = Math.min(progress, 100)
-        progress = Math.max(progress, 0)
-        
-        this.props.onProgressChange(Math.round(progress));
     }
 
     render() {
