a306b0fa52aa601838e6828d9aec87fdf2d6ef07
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-07 16:19:18 +1000

    install eslint and config eslint-react

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: ['babel-loader', 'eslint-loader']
                 // TODO use options and move presets and plugins here ?
             }
         ]
