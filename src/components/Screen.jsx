dd69953a0472ef85f200b5231e232f80323186d9
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-09 23:42:10 +1000

    make Screen a class component

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,22 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function Screen(props) {
-    const {showScreen, toogleFullscreen, fullscreen} = props
-    const { mediaSrc , muted, volume} = props
-    const dragIniter = fullscreen ? "" : "mpl4v-drag-initiator"
-    const hidden = showScreen ? '' : 'mpl4v--hidden'
+export default class Screen extends React.Component {
+    constructor(props) {
+        super(props)
 
-    return (
-    <video 
-        className={`mpl4v-screen ${dragIniter} ${hidden}`}
-        data-fullscreen={ fullscreen }
-        onDoubleClick={ toogleFullscreen }
-        src={ mediaSrc }
-        muted={ muted }
-        volume={ volume }
-    ></video>
-    )
+        this.mediaRef = React.createRef()
+    }
+
+    onDrop = e => {
+        e.preventDefault()
+        console.log('dropped')
+        const data = e.dataTransfer.types.map(x => e.dataTransfer.getData(x))
+        console.log(e.dataTransfer.types)
+        console.log(data)
+    }
+
+    onDragOver = e => {
+        e.preventDefault()
+        console.log('drag over')
+    }
+
+    onDragEnter = e => {
+        e.preventDefault()
+        console.log('drag enter');
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        const video = this.mediaRef.current
+
+        if (this.props.muted != prevProps.muted)
+            video.muted = this.props.muted
+        if (this.props.volume != prevProps.volume)
+            video.volume = this.props.volume
+    }
+
+    render() {
+        const {showScreen, toogleFullscreen, fullscreen} = this.props
+        const { mediaSrc , muted, volume} = this.props
+        const dragIniter = fullscreen ? "" : "mpl4v-drag-initiator"
+        const hidden = showScreen ? '' : 'mpl4v--hidden'
+
+        return (
+        <video 
+            ref={ this.mediaRef }
+            className={`mpl4v-screen ${dragIniter} ${hidden}`}
+            data-fullscreen={ fullscreen }
+            onDoubleClick={ toogleFullscreen }
+            src={ mediaSrc }
+            muted={ muted }
+            volume={ volume }
+            controls
+            onDrop={ this.onDrop }
+            onDragOver={ this.onDragOver }
+            onDragEnter={ this.onDragEnter }
+        ></video>
+        )
+    }
 }
 
 Screen.propTypes = {
