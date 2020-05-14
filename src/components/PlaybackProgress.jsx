51484e27ed3ea0f78ae8f785cdb7aef20a8b1948
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-15 00:51:21 +1000

    update seek bar when mouse is over bar

--- src/components/PlaybackProgress.jsx
+++ src/components/PlaybackProgress.jsx
@@ -2,27 +2,56 @@ import React from "react";
 import PropTypes from 'prop-types'
 import Bar from "./Bar";
 import ProgressBar, { barController } from './ProgressBar'
-import { RefType } from "../utils/utils";
+import { RefType, bound } from "../utils/utils";
+import ProgressCalculator from "../utils/ProgressCalculator";
 
-function PlaybackProgressView(props) {
+class PlaybackProgressView extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-    const { progress, bufferedProgress } = props;
-    const { barEltRef, startSeek, seek } = props
-    const playbackBarClass = seek ? "" : " mpl4v-bar--transition-hor"
+    componentDidMount() {
+        this.calculator = new ProgressCalculator()
+    }
+    
+    getSeekProgress() {
+        return bound(this.calculator.getProgress(), 0, 100)
+    }
 
-    return (
-    <div 
-        className={ `mpl4v-playback-progressbar ${ seek ? "mpl4v-playback-progressbar--seek" : ''}` }
-        ref={ barEltRef }
-        onMouseDown={ startSeek }
-    >
-        <div className={ "mpl4v-playback-progressbar__underlay" }/>
-        <Bar classes={ "mpl4v-bar-buff-color mpl4v-bar--transition-hor"} progress={ bufferedProgress }/>
-        <Bar classes={ "mpl4v-bar-seek-color mpl4v-bar--transition-hor"} progress={ 60 }/>
-        <Bar classes={ `mpl4v-bar-progress-color ${playbackBarClass}`} progress={ progress }/>
-        <ProgressBar.Head classes={ "mpl4v-playback-progressbar__head" } progress={ progress }/>
-    </div>
-    )
+    showSeekBar = e => {
+        this.calculator.init(e, this.props.barEltRef.current, true)        
+        this.setState({ seekProgress: this.getSeekProgress() })
+    }
+    
+    updateSeekBar = e => {
+        this.calculator.update(e)
+        this.setState({ seekProgress: this.getSeekProgress() })
+    }
+
+    render() {
+        const props = this.props
+        const { progress, bufferedProgress } = props;
+        const { barEltRef, startSeek, seek } = props
+        const playbackBarClass = seek ? "" : " mpl4v-bar--transition-hor"
+        const seekProgress = this.state.seekProgress || 0
+    
+        return (
+        <div 
+            className={ `mpl4v-playback-progressbar ${ seek ? "mpl4v-playback-progressbar--seek" : ''}` }
+            ref={ barEltRef }
+            onMouseDown={ startSeek }
+            onMouseOver={ this.showSeekBar }
+            onMouseMove={ this.updateSeekBar }
+        >
+            <div className={ "mpl4v-playback-progressbar__underlay" }/>
+            <Bar classes={ "mpl4v-bar-buff-color mpl4v-bar--transition-hor"} progress={ bufferedProgress }/>
+            <Bar classes={ "mpl4v-bar-seek-color mpl4v-seekbar"} progress={ seekProgress }/>
+            <Bar classes={ `mpl4v-bar-progress-color ${playbackBarClass}`} progress={ progress }/>
+            <ProgressBar.Head classes={ "mpl4v-playback-progressbar__head" } progress={ progress }/>
+        </div>
+        )
+    }
 }
 
 PlaybackProgressView.propTypes = {
