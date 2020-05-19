42c642b02b7b12ede30e736995c38634bf6264f7
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-19 16:57:00 +1000

    install terser-webpack-plugin and config it

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,32 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const config = require('./webpack.config.js')
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(config, {
         mode: 'production',
         optimization: {
-            minimize: false,
+            minimize: true,
+            minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    mangle: false,  // keep vars' names
+                    keep_classnames: true,
+                    keep_fnames: true,
+                    compress: {
+                        ecma: 2016,
+                        evaluate: false,
+                        keep_fnames: true,
+
+                    },
+                    output: {
+                        beautify: true,
+                        comments: false,
+                    }
+                }
+            }),
+            ]
+
         },
         externals: {
             'react': 'React',
