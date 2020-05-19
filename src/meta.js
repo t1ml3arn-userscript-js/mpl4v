226ca02e6f4f97b1f29b5adbbfea1653f26dbbe4
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-19 16:23:35 +1000

    add basic userscript metadata

---
+++ src/meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        New script 
+// @namespace   Violentmonkey Scripts
+// @match       https://example.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @require     https://unpkg.com/react@16.7.0/umd/react.production.min.js
+// @require     https://unpkg.com/react-dom@16.7.0/umd/react-dom.production.min.js
+// @description 5/19/2020, 2:35:49 PM
+// ==/UserScript==
+
