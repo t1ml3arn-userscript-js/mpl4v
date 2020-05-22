3165cfa3afe87515cb581a100117f338f7a95e71
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-22 15:11:45 +1000

    refactoring

--- src/media/Playlist.js
+++ src/media/Playlist.js
@@ -6,31 +6,36 @@ export default class Playlist {
         this.list = list
     }
 
+    get length() { return this.list.length }
+
+    first = () => this.list[0]
+    last = () => this.list[this.length - 1]
+
     getNextTrack(currentSrc = null) {
         let nextInd = 0
 
         if (currentSrc !== null)
             nextInd = this.list.findIndex(x => x.src == currentSrc) + 1
         
-        nextInd = nextInd % this.list.length
+        nextInd = nextInd % this.length
         
         return this.list[nextInd]
     }
 
     getPreventTrack(currentSrc = null) {
-        let prevInd = this.list.length-1
+        let prevInd = this.length-1
 
         if (currentSrc !== null)
             prevInd = this.list.findIndex(x => x.src == currentSrc) - 1
         
         if (prevInd == -2)  return null
 
-        prevInd = prevInd < 0 ? this.list.length - 1 : prevInd
+        prevInd = prevInd < 0 ? this.length - 1 : prevInd
 
         return this.list[prevInd]
     }
 
-    getTrack(src) {
+    findTrack(src) {
         return this.list.find(x => x.src == src) || new Track(src)
     }
 }
