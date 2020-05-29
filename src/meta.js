876718d7c33d2263886c2a2ef74746d0aab5c153
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-05-29 16:44:08 +1000

    bump react version in userscript meta settings

--- src/meta.js
+++ src/meta.js
@@ -5,8 +5,8 @@
 // @grant       none
 // @version     1.0
 // @author      -
-// @require     https://unpkg.com/react@16.7.0/umd/react.production.min.js
-// @require     https://unpkg.com/react-dom@16.7.0/umd/react-dom.production.min.js
+// @require     https://unpkg.com/react@16.13.1/umd/react.production.min.js
+// @require     https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js
 // @description 5/19/2020, 2:35:49 PM
 // ==/UserScript==
 
